0a9ab3c1b9d9d91aae9f67cf092a44430a4b903f
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-05-26 20:18:21 +0900

    add padding to article

--- Handler/Article.hs
+++ Handler/Article.hs
@@ -7,4 +7,5 @@ getArticleR articleId = do
     article <- runDB $ get404 articleId
     defaultLayout $ do
         setTitle $ toHtml $ articleTitle article
+        addStylesheet $ StaticR css_padding_css
         $(widgetFile "article")
