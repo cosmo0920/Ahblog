f86ec6372478c1cb0354557376af4ef1e301fa04
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-05-28 21:24:37 +0900

    add Model Data createdAt and add import Data.Time

--- Model/EntryForm.hs
+++ Model/EntryForm.hs
@@ -3,13 +3,16 @@ module Model.EntryForm where
 import Import
 -- to use Html into forms
 import Yesod.Form.Nic
+import Data.Time
 instance YesodNic App
 entryForm :: Form Article
 entryForm = renderDivs $ Article
   <$> areq textField "Title" Nothing
   <*> areq nicHtmlField "Content" Nothing
+  <*> aformM (liftIO getCurrentTime)
 
 postForm :: Maybe Article -> Form Article
 postForm p = renderDivs $ Article 
   <$> areq textField "Title" (articleTitle <$> p)
   <*> areq nicHtmlField "Content" (articleContent <$> p)
+  <*> aformM (liftIO getCurrentTime)
