20737b25ce126e03c4afc120fd80d823c0ea1ce2
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-06-03 22:27:38 +0900

    change User model

--- Foundation.hs
+++ Foundation.hs
@@ -151,7 +151,7 @@ instance YesodAuth App where
             Just (Entity uid _) -> return $ Just uid
             Nothing -> do
                 fmap Just $ insert $ 
-                  User (credsIdent creds) (credsIdent creds) Nothing
+                  User (credsIdent creds) Nothing Nothing
 
     -- You can add other plugins like BrowserID, email or OAuth here
     authPlugins _ = [authBrowserId, authGoogleEmail]
