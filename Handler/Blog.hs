06b5abc8576025a71fd3704f6db2df8856cfda91
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-06-15 22:03:02 +0900

    remove unused script

--- Handler/Blog.hs
+++ Handler/Blog.hs
@@ -5,6 +5,7 @@ import Yesod.Paginator
 import Helper.MakeBrief
 import Data.Time.Format.Human
 import Data.Time
+
 getBlogViewR :: Handler RepHtml
 getBlogViewR = do
   -- Get the list of articles inside the database
@@ -17,39 +18,3 @@ getBlogViewR = do
   defaultLayout $ do
     setTitle "Internal Blog"
     $(widgetFile "view")
-
-addStyle :: Widget
-addStyle = do
-    -- Twitter Bootstrap
-    addStylesheetRemote "http://netdna.bootstrapcdn.com/twitter-bootstrap/2.1.0/css/bootstrap-combined.min.css"
-    -- message style
-    toWidget [lucius|.message { padding: 10px 0; background: #ffffed; } |]
-    -- jQuery
-    addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"
-    -- delete function
-    toWidget [julius|
-$(function(){
-    function confirmDelete(link) {
-        if (confirm("Are you sure you want to delete this image?")) {
-            deleteImage(link);
-        };
-    }
-    function deleteImage(link) {
-        $.ajax({
-            type: "DELETE",
-            url: link.attr("data-img-url"),
-        }).done(function(msg) {
-            var table = link.closest("table");
-            link.closest("tr").remove();
-            var rowCount = $("td", table).length;
-            if (rowCount === 0) {
-                table.remove();
-            }
-        });
-    }
-    $("a.delete").click(function() {
-        confirmDelete($(this));
-        return false;
-    });
-});
-|]
