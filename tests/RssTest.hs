4d24be1a693992de5409b07736ba0b4ed7e84280
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-07-27 16:10:11 +0900

    add spec /blog/feed when blog has article ver yesod1.1

--- tests/RssTest.hs
+++ tests/RssTest.hs
@@ -4,6 +4,10 @@ module RssTest
     ) where
 
 import TestImport
+import HelperDB
+import qualified Database.Persist as P
+import Data.Time (getCurrentTime)
+import Control.Monad.IO.Class (liftIO)
 
 rssSpecs :: Specs
 rssSpecs =
@@ -12,6 +16,29 @@ rssSpecs =
       it "should be notFound" $ do
         get_ "/blog/feed"
         statusIs 404
-    -- describe "feed can read" $ do
-    --   it "should be ...?" $ do
-    --     pending
+    describe "feed can read" $ do
+      it "should be Found" $ withDeleteArticleTable $ do
+        createTime <- liftIO $ getCurrentTime
+        let title = "test"
+            content = "test post"
+            slug = "testSlug"
+            createdTime = createTime
+        
+        _ <- runDB $ do
+          --when Article has "UserId", then before create User data
+          let email = "test@example.com"
+              name = "test user"
+          userId <- P.insert $ User {
+            userEmail = email
+          , userScreenName = name
+          }
+          P.insert $ Article {
+            articleAuthor = userId
+          , articleTitle = title
+          , articleContent = content
+          , articleSlug = slug
+          , articleCreatedAt = createdTime
+          }
+        --when blog has article, one can get BlogFeedR
+        get_ "/blog/feed"
+        statusIs 200
