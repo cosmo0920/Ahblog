31bbce93c14274e546e6c6a24842eefbf6642faf
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-07-30 21:03:54 +0900

    add comment display spec for yesod 1.1

--- tests/VisitUserTest.hs
+++ tests/VisitUserTest.hs
@@ -51,6 +51,68 @@ visitUserSpecs =
         htmlAllContain "h2" "test"
         htmlAllContain "article" "test post"
 
+    describe "has one content" $ do
+      it "when article has no comment" $ withDeleteCommentTable $ do
+        createTime <- liftIO $ getCurrentTime
+        let title       = "test"
+            content     = "test post"
+            slug        = "testSlug"
+            createdTime = createTime
+
+        _ <- runDB $ do
+          --when Article has "UserId", then before create User data
+          let email = "test@example.com"
+              name  = "test user"
+          userId <- P.insert $ User {
+            userEmail      = email
+          , userScreenName = name
+          }
+          P.insert $ Article {
+            articleAuthor    = userId
+          , articleTitle     = title
+          , articleContent   = content
+          , articleSlug      = slug
+          , articleCreatedAt = createdTime
+          }
+        --when blog has article, one can get BlogArticle
+        get_ "/blog/post/testSlug"
+        statusIs 200
+        htmlAnyContain "h3" "There are no comment in this article"
+
+      it "when article has one comment" $ withDeleteCommentTable $ do
+        createTime <- liftIO $ getCurrentTime
+        let title       = "test"
+            content     = "test post"
+            slug        = "testSlug"
+            createdTime = createTime
+
+        _ <- runDB $ do
+          --when Article has "UserId", then before create User data
+          let email = "test@example.com"
+              name  = "test user"
+          userId <- P.insert $ User {
+            userEmail      = email
+          , userScreenName = name
+          }
+          articleId <- P.insert $ Article {
+            articleAuthor    = userId
+          , articleTitle     = title
+          , articleContent   = content
+          , articleSlug      = slug
+          , articleCreatedAt = createdTime
+          }
+          let cname    = "Anonymous"
+              ccontent = "test comment"
+          P.insert $ Comment {
+            commentName    = cname
+          , commentContent = ccontent
+          , commentArticle = articleId
+          , commentPosted  = createdTime
+          }
+        get_ "/blog/post/testSlug"
+        statusIs 200
+        htmlAnyContain "article" "test comment"
+
     describe "Blog has rss Feed img" $ do
       it "GET /blog then html has img tag which contains feed-icon" $ do
         get_ "/blog"
