e4e82854dd6addbd692f0991628a61c8e82ee368
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-09-21 01:10:55 +0900

    update deploy script

--- deploy/keter-deploy.sh
+++ deploy/keter-deploy.sh
@@ -4,4 +4,6 @@ cabal configure
 cabal build
 strip dist/build/Ahblog/Ahblog
 rm -rf static/tmp
+cp bootstrap-select/bootstrap-select.min.js static/js/bootstrap-select.min.js
+cp bootstrap-select/bootstrap-select.min.css static/css/bootstrap-select.min.css
 tar czfv Ahblog.keter dist/build/Ahblog/Ahblog page config static/{css,files/uploaded_image_dir,img,js}
