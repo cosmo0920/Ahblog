ead5aa7e2006f697508b8326942d67e89bedd595
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-02-08 18:04:50 +0900

    add keter-backup-script

---
+++ tool/ahblog_backup.bash
@@ -0,0 +1,14 @@
+#!/bin/sh -ex
+PRODUCT_NAME=Ahblog
+BACKUP_DIR=/opt/keter/backup
+
+if [ ! -d $BACKUP_DIR ]; then
+  sudo mkdir ${BACKUP_DIR}
+  sudo chown ${USER}:${USER} ${BACKUP_DIR}
+fi
+if [ ! -d $BACKUP_DIR/files ]; then
+  sudo mkdir ${BACKUP_DIR}/files
+fi
+
+cp /opt/keter/temp/${PRODUCT_NAME}-*/*_production.sqlite3 ${BACKUP_DIR}
+cp /opt/keter/temp/${PRODUCT_NAME}-*/static/files/ ${BACKUP_DIR}/files/
