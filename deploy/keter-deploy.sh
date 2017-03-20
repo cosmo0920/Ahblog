6c8272149800fb6f01f9871a67ff20d784f83e01
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-03-20 16:06:51 +0900

    Update deploy script

--- deploy/keter-deploy.sh
+++ deploy/keter-deploy.sh
@@ -2,9 +2,9 @@
 
 stack build
 mkdir -p dist/build/Ahblog/
-cp .stack-work/install/x86_64-linux/lts-3.11/7.10.2/bin/Ahblog dist/build/Ahblog/Ahblog
+cp .stack-work/install/x86_64-linux/lts-8.5/8.0.2/bin/Ahblog dist/build/Ahblog/Ahblog
 strip dist/build/Ahblog/Ahblog
 rm -rf static/tmp
 cp bootstrap-select/bootstrap-select.min.js static/js/bootstrap-select.min.js
 cp bootstrap-select/bootstrap-select.min.css static/css/bootstrap-select.min.css
-tar czfv Ahblog.keter dist/build/Ahblog/Ahblog page config static/{css,files,img,fonts,js}
+tar czfv Ahblog.keter dist/build/Ahblog/Ahblog page config static/{css,files,img,fonts,js} .env
